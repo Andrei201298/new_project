f8cec7a6ce6487d653f57f03e3d2f50602f6ffd6
Andrei <example@gmail.com>
2021-08-12 16:21:36 +0300

    Uslovie IF

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 
-const numberOfFilms = +prompt('How many movies did u watch already, Bob?', '') ;
+// const numberOfFilms = +prompt('How many movies did u watch already, Bob?', '') ;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt('last film u watchaed?',''),
-      b = prompt ('how good it was 0 - 10?', ''),
-      c = prompt('last film u watchaed?',''),
-      d = prompt('how good it was 0 - 10?', '');
+// const a = prompt('last film u watchaed?',''),
+//       b = prompt ('how good it was 0 - 10?', ''),
+//       c = prompt('last film u watchaed?',''),
+//       d = prompt('how good it was 0 - 10?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// if (4==4) {
+//     console.log('Ok!');
+// }else{
+//     console.log('Not ok');
+// }
+
+
+const num = 50;
+
+(num===50) ? console.log('ok') : console.log('not ok');
+
+switch (num) {
+    case 49: 
+        console.log('not correct');
+        break;
+    case 100: 
+        console.log('not correct');
+        break;
+    case 50: 
+        console.log('correct');
+        break;
+}
